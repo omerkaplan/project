dd9aa8440a9272ba0534c89e714ec44dcdeb8e0f
Omer Kaplan <kap.omer@gmail.com>
2017-07-31 10:36:01 -0400

    Smoothed out animation for the header

--- js/main.js
+++ js/main.js
@@ -5,21 +5,12 @@ $(window).scroll(function() {
     var y_scroll_pos = window.pageYOffset;
     var scroll_pos = 300;
     if(y_scroll_pos > scroll_pos) {
-        $('.page-header').css({
-          'padding':'0.5rem 2rem 0.5rem 2rem',
-          'position':'fixed',
-          'width':'100%'
-        });
-        $('.project-name').css({
-          'font-size':'1.5rem',
-          'text-align':'left'
-        });
-        $('.project-tagline').hide();
-        $('.main-content').css('padding-top','80px');
-    }
-    if(y_scroll_pos < scroll_pos)  { //locks the header to top after minimizing
-     $('.page-header').css('position','top');
-  }
+
+      $('.page-header').hide();
+      $('.page-header-scroll').slideDown('slow');
+      $('.main-content').css('padding-top','80px');
+    };
+
 });
 
 // Next cool thing goes below
